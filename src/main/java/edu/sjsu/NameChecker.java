c3f7d8d24fec3c19e3b690fc31a4aea37375ed1a
Moonafdl <fdlmouna@gmail.com>
2024-04-25 03:09:28 +0100

    first commit

--- src/main/java/edu/sjsu/NameChecker.java
+++ src/main/java/edu/sjsu/NameChecker.java
@@ -1,5 +1,7 @@
 package edu.sjsu;
 
+import java.util.regex.Pattern;
+
 public class NameChecker {
     /**
      * Checks if a given string is a valid name following these rules:
@@ -11,8 +13,23 @@ public class NameChecker {
      * @return True if input is a valid name, else false
      */
     public static boolean check(String input) {
-        // TODO: implement
+        // Check length
+        if (input.length() < 2 || input.length() > 40) {
+            return false;
+        }
 
-        return false;
+        if (input.startsWith("-") || input.startsWith("'")) {
+            return false;
+        }
+
+        if (!Pattern.matches("[a-zA-Z'-]+", input)) {
+            return false;
+        }
+
+        if (input.contains("--")) {
+            return false;
+        }
+
+        return true;
     }
 }
